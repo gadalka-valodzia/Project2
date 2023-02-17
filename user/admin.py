5d3efb195678ba4fb9ac980d3a3ec3a90fa351f0
Pavel Vaitsiakhovich 030501 <pavel03.03@inbox.ru>
2023-02-17 21:37:11 +0300

    Добавленая группа

--- user/admin.py
+++ user/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from jedi.inference.value import instance
+
 from  .models import *
 
 # Register your models here.
-@admin.register(User)
+@admin.register(User_reg)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['NSP','login','password','adressat','action_id','prichina_id','group']
+    list_display = ['NSP','login','password','adressat','action_id','prichina_id','groups_g']
     ordering = ['NSP']
     list_per_page = 10
     search_fields = ['NSP', 'self_phone_number', 'home_phone_number', 'passport_number']
     list_filter = ['NSP']
+
--- user/models.py
+++ user/models.py
@@ -1,22 +1,66 @@
-
+import requests
 from django.db import models
 from django.contrib.auth.models import Group, User
+from transliterate import translit
+from django.contrib.contenttypes.models import ContentType
 from list_actions.models import Actions
 from list_operators.models import Operators
 from list_prichini.models import Prichini
+from transliterate.discover import autodiscover
+from transliterate import get_available_language_codes, translit
+from transliterate.base import TranslitLanguagePack, registry
+from django.contrib.auth import get_user_model
+
+autodiscover()
+
+class ExampleLanguagePack(TranslitLanguagePack):
+    language_code = "example"
+    language_name = "Example"
+    mapping = (
+        u"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЫЭЮабвгдеёжзийклмнопрстуфхыэю ",
+        u"ABVGDEEJZIIKLMNOPRSTUFHIEUabvgdeejziiklmnoprstufhieu_",
+
+    )
+    pre_processor_mapping = {
+        u"я": u"ya",
+        u"ь": u"'",
+        u"ъ": u"'",
+        u"ч": u"ch",
+        u"ш": u"sh",
+        u"щ": u"sch"
+
+    }
 
 
+registry.register(ExampleLanguagePack)
 
-class User(models.Model):
+_requests = {}
+
+
+class User_reg(models.Model):
     class Meta:
         verbose_name = 'Линые данные'
         verbose_name_plural = 'Личные данные'
-    NSP = models.CharField(max_length=40,null=True)  # ФИО сотрудника
-    login = models.CharField(max_length=40,null=True) # Логин сотрудника
-    password = models.CharField(max_length=40, null=True) #Пароль
-    adressat = models.ForeignKey(Operators,on_delete=models.CASCADE,null=True) #Адрессат
-    action_id = models.ForeignKey(Actions,on_delete=models.CASCADE,null=True)
-    prichina_id=models.ForeignKey(Prichini,on_delete=models.CASCADE,null=True)
-    group=models.ForeignKey(,on_delete=models.CASCADE,max_length=40,null=True)
+
+    NSP = models.CharField(max_length=40, null=True)  # ФИО сотрудника
+    password = models.CharField(max_length=40, null=True)  # Пароль
+    adressat = models.ForeignKey(Operators, on_delete=models.CASCADE, null=True)  # Адрессат
+    action_id = models.ForeignKey(Actions, on_delete=models.CASCADE, null=True)
+    prichina_id = models.ForeignKey(Prichini, on_delete=models.CASCADE, null=True)
+    login = models.CharField(max_length=40, null=True, blank=True, editable=False)
+
+    def groups_g(self):
+        a = User.objects.all()
+        for b in a:
+             if b.is_authenticated:
+                 return b.groups.all().get().name
+    def login_set(self):
+        arg = u"{}_{[0]}{[0]}".format(*(translit(self.NSP.lower(), 'example')).split('_'))
+        return arg
+
+    def save(self, *args, **kwargs):
+        self.login = self.login_set()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return f'{self.NSP}'
